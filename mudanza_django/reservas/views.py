bcc49b009b604783464a15214325bbe1b0fb3367
Daniel Henriquez <daniel.henriquez.v@hotmail.com>
2025-11-28 22:17:08 +0100

    Se arreglan problemas con el settings y algunos otros que no permitian ver la url de reservas.

--- mudanza_django/reservas/views.py
+++ mudanza_django/reservas/views.py
@@ -1,6 +1,20 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from .forms import ReservaForm
+from .models import Reserva
+
+
+@login_required
+def lista_reservas(request):
+    """
+    Vista que muestra todas las reservas registradas.
+    """
+    reservas = Reserva.objects.select_related('cliente').all().order_by('fecha_mudanza', 'hora_mudanza')
+    context = {
+        'reservas': reservas
+    }
+    return render(request, 'reservas/lista_reservas.html', context)
+
 
 @login_required
 def crear_reserva(request):
@@ -14,4 +28,5 @@ def crear_reserva(request):
     else:
         form = ReservaForm()
     return render(request, 'reservas/crear_reserva.html', {'form': form})
+
 
