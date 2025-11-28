d673bb2cf5bc9626cf56a9019169808e183a8f9d
Daniel Henriquez <daniel.henriquez.v@hotmail.com>
2025-11-28 22:50:27 +0100

    Se mejora la parte visual, se agrega un base.html Se agregan nuevos statics

--- mudanza_django/mudanza_site/settings.py
+++ mudanza_django/mudanza_site/settings.py
@@ -116,7 +116,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = "static/"
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
