89898671beb8031b3e3b641aee90500f0afbcea4
Daniel Henriquez <daniel.henriquez.v@hotmail.com>
2025-11-28 21:53:05 +0100

    Implementar app Django usando templates para desplegar contenido dinámico desde la BD

---
+++ mudanza_django/reservas/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+app_name = 'reservas'
+
+urlpatterns = [
+    path('', views.lista_reservas, name='lista_reservas'),
+]
--- mudanza_django/reservas/views.py
+++ mudanza_django/reservas/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from .models import Reserva
 
-# Create your views here.
+def lista_reservas(request):
+    reservas = Reserva.objects.select_related('cliente').all().order_by('fecha_mudanza', 'hora_mudanza')
+    return render(request, 'reservas/lista_reservas.html', {'reservas': reservas})
