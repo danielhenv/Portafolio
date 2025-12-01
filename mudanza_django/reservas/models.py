ce2495fd6b07fa5bd4584aa063a47c68091c3283
Daniel Henriquez <daniel.henriquez.v@hotmail.com>
2025-12-01 14:15:51 +0100

    Agrego modelos Producto, PerfilCliente, Pedido y DetallePedido con migraciones

--- mudanza_django/reservas/models.py
+++ mudanza_django/reservas/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.conf import settings
 
 class Cliente(models.Model):
     nombre = models.CharField(max_length=100)
@@ -28,4 +29,45 @@ class Reserva(models.Model):
 
     def __str__(self):
         return f"Reserva #{self.id} - {self.cliente.nombre}"
+    
+
+class Producto(models.Model):
+    nombre = models.CharField(max_length=100)
+    precio = models.DecimalField(max_digits=7, decimal_places=2)
+    cantidad = models.PositiveIntegerField()
+
+    def __str__(self):
+        return f"{self.nombre} (${self.precio})"
+    
+#Relacion OnetoOne
+class PerfilCliente(models.Model):
+    cliente = models.OneToOneField(Cliente, on_delete=models.CASCADE, related_name='perfil')
+    biografia = models.TextField(blank=True)
+    foto_perfil = models.URLField(blank=True)
+    redes_sociales = models.CharField(max_length=255, blank=True)
+
+    def __str__(self):
+        return f"Perfil de {self.cliente.nombre}"
+    
+
+#Relacion ManytoMany
+
+class Pedido(models.Model):
+    cliente = models.ForeignKey(Cliente, on_delete=models.CASCADE, related_name='pedidos')
+    fecha = models.DateTimeField(auto_now_add=True)
+    notas = models.TextField(blank=True)
+
+    def __str__(self):
+        return f"Pedido #{self.id} de {self.cliente.nombre}"
+
+
+class DetallePedido(models.Model):
+    pedido = models.ForeignKey(Pedido, on_delete=models.CASCADE, related_name='detalles')
+    producto = models.ForeignKey(Producto, on_delete=models.CASCADE, related_name='detalles')
+    cantidad = models.PositiveIntegerField()
+    precio_unitario = models.DecimalField(max_digits=7, decimal_places=2)
+
+    def __str__(self):
+        return f"{self.cantidad} x {self.producto.nombre} en pedido #{self.pedido.id}"
+
 
